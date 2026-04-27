xiota/dolphin-primehack

Add all UI changes

Source/Core/DolphinQt/Settings/GeneralPane.h
@@ -42,6 +42,7 @@ private:
   QComboBox* m_combobox_fallback_region;
   QCheckBox* m_checkbox_dualcore;
   QCheckBox* m_checkbox_cheats;
+  QCheckBox* m_checkbox_primehack;
   QCheckBox* m_checkbox_override_region_settings;
   QCheckBox* m_checkbox_auto_disc_change;
 #ifdef USE_DISCORD_PRESENCE

AluminiumBromid/dolphin

Source/Core/DolphinQt/Settings/GeneralPane.h
@@ -51,6 +51,7 @@ private:
   ConfigBool* m_checkbox_load_games_into_memory;
   ConfigBool* m_checkbox_override_region_settings;
   ConfigBool* m_checkbox_auto_disc_change;
+  ConfigBool* m_checkbox_primehack;
 #ifdef USE_DISCORD_PRESENCE
   ToolTipCheckBox* m_checkbox_discord_presence;
 #endif